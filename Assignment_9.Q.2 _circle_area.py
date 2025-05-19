b04d5523f0bf06fa0fb37a297196e7a6b9611240
Pournimanikam21 <pournimanikam1935@gmail.com>
2025-05-19 09:01:19 +0530

    Add files via upload

---
+++ Assignment_9.Q.2 _circle_area.py
@@ -0,0 +1,20 @@
+# Write a program to calculate following,
+
+#    The area of a circle
+#    The circumference of a circle
+#    The area of a rectangle
+#    The perimeter of a rectangle
+
+# You have to create two separate modules one for functions of circle and one for function of rectangle, and use these modules in another program.
+
+# Pournima Kiran Nikam.
+# FY B 
+# Roll No. 117
+
+import math
+
+def circle_area(radius):
+    return math.pi * radius**2
+
+def circle_circumference(radius):
+    return 2 * math.pi * radius
---
+++ Assignment_9.Q.2 _rectangle_area.py
@@ -0,0 +1,20 @@
+# Write a program to calculate following,
+
+#    The area of a circle
+#    The circumference of a circle
+#    The area of a rectangle
+#    The perimeter of a rectangle
+
+# You have to create two separate modules one for functions of circle and one for function of rectangle, and use these modules in another program. 
+
+# Pournima Kiran Nikam.
+# Roll No. 117
+# FY B
+
+import math
+
+def rectangle_area(length, width):
+    return length * width
+
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
